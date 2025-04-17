100af43a65741dc6c791e5eb7132d28f1f0703ab
Rajesh <rajeshsathish24@gmail.com>
2025-04-17 15:21:38 +0530

    first file of java

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main (String args[]){
+
+        int a = 1;
+        while(a<=5){
+            System.out.println("hello");
+            a++;
+        }
+    }
+}
